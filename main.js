2c4593dc9e407f377095e288857c860e7b584cdf
Varkey-Josu <varkeyjosu2003@gmail.com>
2024-07-05 21:16:19 +0530

    responsive navbar

---
+++ main.js
@@ -0,0 +1,10 @@
+function on(){
+    document.getElementById("contain").style.display="block";
+    document.getElementById("hamburger").style.display="none";
+    document.getElementById("cross").style.display="block";
+}
+function off(){
+    document.getElementById("contain").style.display="none";
+    document.getElementById("hamburger").style.display="block";
+    document.getElementById("cross").style.display="none";
+}
